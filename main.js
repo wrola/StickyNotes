866ddb1221c41b96164723dec8456f0a57b8f79a
wrola <rola.wojciech@gmail.com>
2018-04-23 16:25:19 +0200

    notes works

--- main.js
+++ main.js
@@ -2,33 +2,28 @@
     'use strict';
 
     var draggedEl,
-    onDragStart,
-    onDrag,
-     onDragEnd,
-    grabPointY,
-    grabPointX,
-    createNote,
-    addNoteBtnEl;
+        onDragStart,
+        onDrag,
+        onDragEnd,
+        grabPointY,
+        grabPointX,
+        createNote,
+        addNoteBtnEl;
 
     onDragStart = function (ev) {
         var boundClientRect;
-        if (ev.target.class.index('bar'=== -1)){
+        if (ev.target.className.indexOf('bar')=== -1) {
         return;
         }
 
         draggedEl = this;
 
-        boundClientRect = draggedEl.getBoundingClientRect()
+        boundClientRect = draggedEl.getBoundingClientRect();
 
         grabPointY = boundClientRect.top - ev.clientY;
         grabPointX = boundClientRect.left - ev.clientX;
-        if (posX < 0){
-            posX = 0;
-        }
-        if(posY < 0){
-            posY = 0;
-        }
     };
+
     onDrag = function (ev) {
         if(!draggedEl){
             return;
@@ -37,18 +32,29 @@
         var posX = ev.clientX + grabPointX,
             posY = ev.clientY + grabPointY;
 
-        draggedEl.style.transform = 'translateX('+ posX + 'px) translateY('+posY+'px)';
+        if (posX < 0){
+            posX = 0;
+        }
+        if(posY < 0){
+            posY = 0;
+        }
+
+        draggedEl.style.transform = 'translateX('+ posX + 'px) translateY('+ posY +'px)';
 
     };
+
     onDragEnd = function () {
         draggedEl =  null;
         grabPointY = null;
         grabPointX = null;
     };
+
+    // Make new note
     createNote = function () {
         var stickerEl = document.createElement('div'),
             barEl = document.createElement('div'),
             textareaEl = document.createElement('textarea');
+
         var transformCSSValue = 'translateX('+Math.random()*400+'px)translateY('+Math.random() *400+'px)';
         stickerEl.style.transform = transformCSSValue;
 
@@ -58,6 +64,7 @@
         stickerEl.appendChild(barEl);
         stickerEl.appendChild(textareaEl);
         stickerEl.addEventListener('mousedown', onDragStart, false);
+
         document.body.appendChild(stickerEl);
     };
     createNote();
@@ -65,7 +72,6 @@
     addNoteBtnEl = document.querySelector('.addNoteBtn');
     addNoteBtnEl.addEventListener('click',createNote,false);
     document.addEventListener('mousemove',onDrag,false);
-    document.querySelector('.sticker').addEventListener('mousedown',onDragStart, false);
-    document.addEventListener()
+    document.addEventListener('mouseup', onDragEnd, false);
 
 })();
